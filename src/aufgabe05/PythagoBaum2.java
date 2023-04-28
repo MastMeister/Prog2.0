12ee4dbd83ec7bed1b07364c9d5391ee3ce49a97
MastMeister <erbeugen@gmail.com>
2023-04-28 15:37:04 +0200

    Aufgabe05 Baum2 funktioniert auch!!!!!

--- src/aufgabe05/PythagoBaum2.java
+++ src/aufgabe05/PythagoBaum2.java
@@ -1,7 +1,9 @@
 package aufgabe05;
+import java.util.Random;
 
-public class PythagoBaum1 {
-    private static double g = 45; // Der Winkel der Abzweigung Winkel bis 90 grad möglich theorethisch
+public class PythagoBaum2 {
+    //private static double g = 45; // Der Winkel der Abzweigung Winkel bis 90 grad möglich theorethisch
+    public static final double SkalierungDickeLaenge = 3.0;
 
     public static void main(String[] args) {
         double omega = 30;
@@ -10,55 +12,61 @@ public class PythagoBaum1 {
         StdDraw.setYscale(0,1080);
         StdDraw.setPenRadius(.005);
         // rotiert mit winkel 30°
-       StdDraw.show(0);
-       int i = 3;
+        StdDraw.show(0);
 
-       test(0,100,960,50);  // Startkoordinaten des Baumes und die Länge der Quadrate
-       StdDraw.show(0);
+        test(0,100,150,960,50);  // Startkoordinaten des Baumes und die Länge der Quadrate
+        StdDraw.show(0);
 //        test(0,100);
 //        StdDraw.show(0);
 
 
     }
-    private static void test(double w, double l,double ax,double ay){
+    private static void test(double w, double l,double he,double ax,double ay){
+        double g = (int) ((Math.random() *76)+10);
         System.out.println("Winkel :" + w+"\n Länge :" + l);
-        if(l< 1)
+        if(l< 2 ) {
             return;
+        }
         if(l < 5 )
             StdDraw.setPenColor(StdDraw.GREEN);
         else
-        StdDraw.setPenColor(StdDraw.ORANGE);
+            StdDraw.setPenColor(StdDraw.ORANGE);
         StdDraw.setPenRadius(.005);
         double winkel = Math.toRadians(w);
         double omega = Math.toRadians(g);
         double lange = l;
+        double hoehe = he;
         double AX = ax;
         double AY = ay;
         double c = lange * Math.cos(winkel);
         double s = lange * Math.sin(winkel);
+        double ch = hoehe* Math.cos(winkel);
+        double sh = hoehe* Math.sin(winkel);
         double BX = AX + c;
         double BY = AY + s;
-        double CX = AX + c - s;
-        double CY = AY + s + c;
-        double DX = AX - s;
-        double DY = AY + c;
+        double CX = AX + c - sh;
+        double CY = AY + s + ch;
+        double DX = AX - sh;
+        double DY = AY + ch;
         double u = lange*Math.cos(omega );
         double v = lange*Math.sin(omega );
         double EX = DX + u*(Math.cos(winkel + omega));
         double EY = DY + u*Math.sin(winkel + omega);
 
-        StdDraw.line(AX, AY, BX, BY);
+       // StdDraw.line(AX, AY, BX, BY);
         StdDraw.line(AX, AY, DX, DY);
-        StdDraw.line(CX, CY, DX, DY);
+       // StdDraw.line(CX, CY, DX, DY);
         StdDraw.line(CX, CY, BX, BY);
+
 //        StdDraw.setPenColor(StdDraw.BLUE);
 //        StdDraw.point(AX,AY);
 //        StdDraw.point(BX,BY);
 //        StdDraw.setPenRadius(.009);
 //        StdDraw.setPenColor(StdDraw.BOOK_RED);
 //        StdDraw.point(EX,EY);
-            test(w-(90 - g),v ,EX,EY);
-            test(w+g,u ,DX,DY);
+        double h = (int) (Math.random() *(SkalierungDickeLaenge*l)+1);
+        test(w+g ,u ,h,DX,DY);
+        test(w-(90 - g),v,h,EX,EY);
 
     }
 //    private static void test2(double w, double l,double ax,double ay){
